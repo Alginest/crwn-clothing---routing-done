6fae75c10cddaa005b96ff43afec02c09b8cb213
ado durak <76266634+Alginest@users.noreply.github.com>
2021-12-12 19:21:13 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import Homepage from "./pages/homepage/Homepage";
+import { Route, Switch } from "react-router-dom";
+import ShopPage from "./pages/shop/ShopPage";
+function App() {
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/shop" component={ShopPage} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
